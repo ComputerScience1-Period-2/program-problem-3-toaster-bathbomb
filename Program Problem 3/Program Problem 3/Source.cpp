b941493daaf2eff6f81821226adab06cf22186a8
Sumadhwa13 <31855554+Sumadhwa13@users.noreply.github.com>
2017-10-12 10:42:21 -0500

    Assignment 4
    Sumadhwa Guruprasad - 10/12/2017

--- Program Problem 3/Program Problem 3/Source.cpp
+++ Program Problem 3/Program Problem 3/Source.cpp
@@ -5,7 +5,7 @@ Three Digit Ascending and Descending Selection
 
 #include <iostream>
 #include <conio.h>
-#include <Windows.h>
+
 
 
 using namespace std;
@@ -22,31 +22,47 @@ void pause() {
 void main() {
 	int three_digit_number;
 
-
 	cout << "Enter a three digit number, ";
-	Sleep(2000);
 	cout << "please. : ";
 	cin >> three_digit_number;
 
 
-	int x = three_digit_number;
-	int A = x / 100;
-	int B = (x / 10) % 10;
-	int C = x % 10;
+	//don't try to change the variable name, it could confuse you later on; stick to the specific variable name you've already given
+	int A = three_digit_number / 100;
+	int B = (three_digit_number / 10) % 10;
+	int C = three_digit_number % 10;
 
 
 	if (A < B && B < C) {
 		cout << "Ascending..." << endl;
-		Sleep(2000);
 	}
 	else if (A > B && B > C) {
 		cout << "Descending..." << endl;
-		Sleep(2000);
 	}
 	else {
 		cout << "Neither..." << endl;
-		Sleep(2000);
 	}
+	
+	for (int i = 1; i < 30; i++) {
+		cout << "Enter a three digit number, ";
+	cout << "please. : ";
+	cin >> three_digit_number;
+
+	int A = three_digit_number / 100;
+	int B = (three_digit_number / 10) % 10;
+	int C = three_digit_number % 10;
+		
+		if (A < B && B < C) {
+		cout << "Ascending..." << endl;
+	}
+	else if (A > B && B > C) {
+		cout << "Descending..." << endl;
+	}
+	else {
+		cout << "Neither..." << endl;
+	}
+	}
+		
 
 	pause();
 }
